2bf59e7425c19aab56a88c6f8e8ccd5c5d317563
parshva1510 <parshva@teque7.com>
2023-07-26 11:43:07 +0530

    depo

--- app/Http/Controllers/Expense.php
+++ app/Http/Controllers/Expense.php
@@ -3,10 +3,68 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Mahajan_Expense;
+use App\Models\add_members;
+
+use Carbon\Carbon;
 
 class Expense extends Controller
 {
-    public function Expense_Receipt(){
-        return view ('Expense.Expense_Receipt');
+
+    public function index(){
+
+        $m_name =(Mahajan_Expense::get()->last()->m_name);
+        $p_details=Mahajan_Expense::with('member')->get();
+        $m_data = add_members::all();
+        $id=add_members::get()->last()->id;
+        $depo_id = Mahajan_Expense::get()->last()->depo_id;
+        //dd($depo_id);
+        
+
+        return view ('Expense.Mahajan_Expense',['m_name' => $m_name, 'p_details' => $p_details, 'id' => $id, 'm_data' => $m_data,'depo_id'=>$depo_id ]);
+    }
+
+
+    public function Mahajan_Expense(Request $req){
+
+        $m_name =(Mahajan_Expense::get()->last()->m_name);
+       
+        $p_details=Mahajan_Expense::with('member')->get();
+        $m_data = add_members::all();
+        $data = Mahajan_Expense::find($req->depo_id);
+        $Mahajan_Expense = new Mahajan_Expense();
+        //Personal Details
+        $Mahajan_Expense -> depo_id  = $req -> depo_id;
+        $Mahajan_Expense-> date = Carbon::now();
+        $Mahajan_Expense -> name = $req -> name;
+        $Mahajan_Expense -> details = $req -> collapsible_details;
+        $Mahajan_Expense -> amount = $req -> amount;
+        $Mahajan_Expense -> inword = $req -> inword;
+        $Mahajan_Expense -> save();
+        $depo_id = (Mahajan_Expense::get()->last()->depo_id)+1;
+       
+
+        //$m_data= DB::select("select * from member_details ORDER BY add_members.p_id DESC");
+        return view ('Expense.Mahajan_Expense',['p_details'=>$p_details,'m_name'=>$m_name,'m_data'=>$m_data, 'data'=>$data,'depo_id'=>$depo_id ]);
+
+        // return view ('Donation.Mahajan_Expense');
+    }
+
+    public function Sangh_Expense(){
+        return view ('Expense.Sangh_Expense');
+    }
+
+    public function add_member(Request $req){
+        
+        $p_details= Mahajan_Expense::with('member')->get();
+        $member = new add_members();
+        $member->m_name = strtoupper($req->m_name);
+        $member->email = $req->email;
+        $member->phone_no = $req->phone_no;
+        $member->city = $req->city;
+        $member->address = strtoupper($req->address);
+        $member->save();
+        $m_data=add_members::all();
+        return view('Expense.Mahajan_Expense',['member'=>$member,'m_data'=>$m_data,'p_details'=>$p_details]);
     }
 }
--- app/Models/Mahajan_Expense.php
+++ app/Models/Mahajan_Expense.php
@@ -5,9 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class General_Donation extends Model
+class Mahajan_Expense extends Model
 {
     use HasFactory;
-    protected $table = 'GeneralDonation';
+    protected $table = 'Mahajan_Expense';
     public $timestamps = false;
+
+    public function member()
+    {
+        return $this->belongsTo(add_members::class, 'id');
+    }
 }
