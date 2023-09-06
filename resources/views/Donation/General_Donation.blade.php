097abffd7f5d84b78f96bdd64aeba5e4718c2ef9
rupalteque7 <rupal@teque7.com>
2023-09-06 17:48:58 +0530

    validation

--- resources/views/Donation/General_Donation.blade.php
+++ resources/views/Donation/General_Donation.blade.php
@@ -292,12 +292,9 @@
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
   const forms = document.querySelectorAll('.needs-validation');
-
   // Loop over them and prevent submission
   Array.prototype.slice.call(forms).forEach((form) => {
- 
     form.addEventListener('submit', (event) => {
-      alert("hi");
       if (!form.checkValidity()) {
         
         event.preventDefault();
